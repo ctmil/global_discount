fbdba69c93c40284f16d0e8a83d4d309b82bd7fa
morrillo <gustavo.orrillo@gmail.com>
2017-03-15 13:10:29 -0300

    AAdded onstraint

--- models.py
+++ models.py
@@ -22,6 +22,18 @@ class purchase_order(models.Model):
 	#	if self.product_qty > 0:
 	#		self.price_unit = self.price_subtotal / self.product_qty
 
+	@api.onchange('discount')
+	def onchange_po_discount(self):
+		if self.discount > 0 and self.discount < 100:
+			for line in self.order_line:
+				line.write({'discount': self.discount})
+
+	@api.one
+	@api.constrains('discount')
+	def _check_discount(self):
+		if self.discount < 0 or self.discount > 99.99:
+			raise ValidationError("Descuento debe ser mayor a 0 y menor a 100")
+
 	discount = fields.Float('Descuento')
 
 
